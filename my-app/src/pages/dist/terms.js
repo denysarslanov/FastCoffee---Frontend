d448d181942376cd13beecafdbbc184a46ba3088
DenysArslanov <denarslwork@gmail.com>
2024-12-25 12:43:21 +0100

    Added footer to Terms and Conditions page

--- my-app/src/pages/dist/terms.js
+++ my-app/src/pages/dist/terms.js
@@ -4,9 +4,11 @@ var React = require("react");
 var NavbarWrapper_1 = require("../components/ordinary/NavbarWrapper/NavbarWrapper");
 var Termas_1 = require("../components/simple/Terms/Termas");
 var constants_1 = require("../core/constants");
+var Footer_1 = require("../components/ordinary/Footer/Footer");
 var TermsPage = function () {
     return React.createElement(React.Fragment, null,
         React.createElement(NavbarWrapper_1["default"], { black: true }),
-        React.createElement(Termas_1["default"], { terms: constants_1.termsAndConditions }));
+        React.createElement(Termas_1["default"], { terms: constants_1.termsAndConditions }),
+        React.createElement(Footer_1["default"], null));
 };
 exports["default"] = TermsPage;
--- my-app/src/pages/terms.tsx
+++ my-app/src/pages/terms.tsx
@@ -3,11 +3,13 @@ import Navbar from '../components/simple/Navbar/Navbar'
 import NavbarWrapper from '../components/ordinary/NavbarWrapper/NavbarWrapper'
 import Terms from '../components/simple/Terms/Termas'
 import { termsAndConditions } from '../core/constants'
+import Footer from '../components/ordinary/Footer/Footer'
  
 const TermsPage : React.FC<TermsProps> = () => {
     return <>
         <NavbarWrapper black={true} />
         <Terms terms={termsAndConditions} />
+        <Footer />
     </>
 }
 
